6766c2fa188117224a77ace8b7c709154620f3c8
Ejan <ezan.shakya@gmail.com>
2022-09-02 10:54:02 -0500

    sample neuron test

--- sample_neuron.py
+++ sample_neuron.py
@@ -1,29 +1,9 @@
-from neuron import h, gui
-from neuron.units import um
-import plotly
-h.load_file("stdrun.hoc")
+from neuron import h
+soma = h.Section(name='soma')
+axon = h.Section(name='axon')
+dend = [h.Section(name='dend[%d]' % i) for i in range(3)]
+for sec in dend:
+    sec.connect(soma(1), 0)
 
-
-class Cell:
-    def __int__(self):
-        main = h.Section(name="main", cell=self)
-        dend1 = h.Section(name="dend1", cell=self)
-        dend2 = h.Section(name="dend2", cell=self)
-
-        dend1.connect(main)
-        dend2.connect(main)
-
-        main.diam = 10
-        dend1.diam = 2
-        dend2.diam = 2
-
-        # important: store the sections
-        self.main = main
-        self.dend1 = dend1
-        self.dend2 = dend2
-        self.all = main.wholetree()
-
-
-myCell = Cell()
-ps = h.PlotShape(False)
-ps.plot(plotly).show()
+h.topology()
+s = h.Shape()
